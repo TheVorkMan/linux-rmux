4bf22dc42ab7010929539ac00357cf130a22c31c
Niels Westphal (crashniels) <nielswest@live.de>
2025-07-11 17:08:47 +0200

    Proper way to add XHCI_PLAT_DMA

--- drivers/usb/host/xhci.h
+++ drivers/usb/host/xhci.h
@@ -1642,9 +1642,10 @@ struct xhci_hcd {
 #define XHCI_TRB_OVERFETCH	BIT_ULL(45)
 #define XHCI_ZHAOXIN_HOST	BIT_ULL(46)
 #define XHCI_WRITE_64_HI_LO	BIT_ULL(47)
-#define XHCI_CDNS_SCTX_QUIRK	BIT_ULL(48)
-#define XHCI_ETRON_HOST	BIT_ULL(49)
+#define XHCI_CDNS_SCTX_QUIRK    BIT_ULL(48)
+#define XHCI_ETRON_HOST BIT_ULL(49)
 #define XHCI_LIMIT_ENDPOINT_INTERVAL_9 BIT_ULL(50)
+#define XHCI_PLAT_DMA       BIT_ULL(51)
 
 	unsigned int		num_active_eps;
 	unsigned int		limit_active_eps;
