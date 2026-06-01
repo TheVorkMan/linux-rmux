4668a1b85d8e789903cbd2aaac95065f63c20321
rmxunet <armandas.kvietkus@proton.me>
2026-06-01 12:46:12 +0000

    usb: host: Add xHCI driver for Sony PlayStation 4
    Signed-off-by: rmxunet <armandas.kvietkus@proton.me>

---
+++ drivers/usb/host/xhci-ps4.h
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * xhci-ps4.h - xHCI host controller driver for Sony PS4 (Aeolia/Belize/Baikal)
+ */
+
+#ifndef DRIVERS_USB_HOST_XHCI_PS4_H_
+#define DRIVERS_USB_HOST_XHCI_PS4_H_
+
+#include "../../ata/ahci.h"
+#include <linux/irqdomain.h>
+
+#endif /* DRIVERS_USB_HOST_XHCI_PS4_H_ */
