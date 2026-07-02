51ef9088d12c6dd10935deffb8f8fa0357126729
rmuxnet <armandas.kvietkus@proton.me>
2026-07-02 11:48:50 +0000

    drm/amdgpu: restore CHIP_LIVERPOOL and CHIP_GLADIUS in device init paths

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -121,6 +121,8 @@ const char *amdgpu_asic_name[] = {
 	"KAVERI",
 	"KABINI",
 	"HAWAII",
+	"LIVERPOOL",
+	"GLADIUS",
 	"MULLINS",
 	"TOPAZ",
 	"TONGA",
@@ -1969,6 +1971,8 @@ static int amdgpu_device_ip_early_init(struct amdgpu_device *adev)
 	case CHIP_KAVERI:
 	case CHIP_KABINI:
 	case CHIP_MULLINS:
+	case CHIP_LIVERPOOL:
+	case CHIP_GLADIUS:
 		if (adev->flags & AMD_IS_APU)
 			adev->family = AMDGPU_FAMILY_KV;
 		else
@@ -6857,6 +6861,8 @@ bool amdgpu_device_has_display_hardware(struct amdgpu_device *adev)
 	case CHIP_KAVERI:
 	case CHIP_KABINI:
 	case CHIP_MULLINS:
+	case CHIP_LIVERPOOL:
+	case CHIP_GLADIUS:
 #endif
 	case CHIP_TONGA:
 	case CHIP_FIJI:
